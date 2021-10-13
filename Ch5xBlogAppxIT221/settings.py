ed4bd8a67af2f51d4824713760bc786a3d1e1a5e
Danny Channita <danny.channita@snhu.edu>
2021-10-13 12:46:51 -0400

    ch 7,just completed the signup section. This allows any user to create a login.

--- Ch5xBlogAppxIT221/settings.py
+++ Ch5xBlogAppxIT221/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,5 @@ STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
+
+LOGOUT_REDIRECT_URL = 'home'
