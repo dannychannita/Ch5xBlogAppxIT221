435a146f42f539e3726eda3d68792238732fc54d
Danny Channita <danny.channita@snhu.edu>
2021-10-13 13:24:35 -0400

    ch 7 just finished heroku config, and now ready to start section: heroku development

--- Ch5xBlogAppxIT221/settings.py
+++ Ch5xBlogAppxIT221/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l5hxx1f1gh*y#_vge!bqzf*d9bp9qr5!a(&awgqtj-g)5e2ofh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
